dc9b0ce492ea3e6463cb4c2fa404f3d3158aed98
DRVAmarnath2004 <amarnath.22bce8654@vitapstudent.ac.in>
2026-05-22 11:12:27 +0530

    feat:1st Upload2

--- Amar.py
+++ Amar.py
@@ -4,7 +4,22 @@
 #     print()
 
 
-def fun(x,y):
-    print(x,y)
-    return x+y
-print(fun(10,20))
+# def fun(x,y):
+#     print(x,y)
+#     return x+y
+# print(fun(10,20))
+
+# def great(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# print(great(75,77))
+
+def fun(*a):
+    return sum(a)
+x=fun(1,7,8,6,5,3,2,8)
+if x%2==0:
+    print(f"even:{x}")
+else:
+    print(f"odd:{x}")
