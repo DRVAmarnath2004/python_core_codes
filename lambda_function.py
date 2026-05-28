02b68ac2d3be7a7481a5eb8e4b13746c78cd832f
DRVAmarnath2004 <amarnath.22bce8654@vitapstudent.ac.in>
2026-05-28 10:33:57 +0530

    feat:1st Upload5

--- lambda_function.py
+++ lambda_function.py
@@ -34,3 +34,32 @@
 # k = list(filter(lambda x: x not in v, a))
 # print(k)#['m', 'r']
 
+# l=[1,7,8,12,14,21,22,63,66]
+# e=list(map(lambda x:x**3 ,l))
+# k=list(filter(lambda x:x%4,e))
+# print(k)#[1, 343, 9261, 250047]
+# print(e)#[1, 343, 512, 1728, 2744, 9261, 10648, 250047, 287496]
+
+#-----if we want in a single statement of the above one then-----
+# l=[1,7,8,12,14,21,22,63,66]
+# e=list(filter(lambda x:x%4,map(lambda x:x**3,l)))
+# print(e)#[1, 343, 9261, 250047]
+
+#----------3.Reduce----------------------
+
+from functools import reduce
+# l=[1,7,8,12,14,21,22,63,66]
+# m=reduce(lambda x,y:x+y,l)
+# print(m)
+
+#------Sorted------------------------------
+# l=[21,3,2,5,22,6,32]
+# k=sorted(l,key=lambda x:x%3)
+# print(k)#[21, 3, 6, 22, 2, 5, 32]
+
+# l=[23,21,27,28,44,46]
+# k=sorted(l,key=lambda x:x%7,reverse=True)
+# m=sorted(l,key=lambda x:x%7,reverse=False)
+# print(k)#[27, 46, 23, 44, 21, 28] this value is occur is reverse is true means descending order of remainders
+# print(m)#[21, 28, 23, 44, 46, 27] this value is occur is reverse is false means ascending order of remainders
+
