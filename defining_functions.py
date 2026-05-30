ee8b306389815e1dbd0f13c6e562d957fd3632c3
DRVAmarnath2004 <amarnath.22bce8654@vitapstudent.ac.in>
2026-05-30 10:45:36 +0530

    feat:1st Upload11

---
+++ defining_functions.py
@@ -0,0 +1,22 @@
+#1.
+def say_hello(a):
+    return a
+
+print(say_hello("Welcome to python"))#Welcome to python
+
+#2.
+def add(a,b):
+    return a+b
+print(add(10,20))#30
+
+#3.
+def test():
+    # print("Hello")
+result = test()
+print(result)#Hello None
+
+#4.
+def area_of_rect(length,breadth):
+    return length*breadth
+print(area_of_rect(10,20))#200
+
