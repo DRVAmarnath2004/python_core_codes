451c53d27b920665f1c46b53ff100728cc6df869
DRVAmarnath2004 <amarnath.22bce8654@vitapstudent.ac.in>
2026-06-02 11:03:37 +0530

    feat:1st Upload15

--- Keyword_Arguments.py
+++ Keyword_Arguments.py
@@ -1,31 +1,31 @@
-#1.
-def send_email(to,subject,body):
-    print(f"{to} {subject} {body}")
-send_email("amar",body="hi",subject="hello")#amar hello hi
-
-#2.
-def create_profile(username,email,age):
-    print(f"{username} {email} {age}")
-create_profile("amar2004",email="drvamarnath2004",age=21)#amar2004 drvamarnath2004 21
-
-#3.
-def create_profile(username,email,age):
-    print(f"{username} {email} {age}")
-create_profile(email="drvamarnath2004",age=21,amar2004)#SyntaxError: positional argument follows keyword argument
-
-4.
-def book_ticket(mem1,mem2,mem3,num):
-    print(f"{mem1} {mem2} {mem3} {num}")
-book_ticket(mem1="Alice",mem2="Delhi",mem3="Mumbai",num=2)#Alice Delhi Mumbai 2
-
-#5.
-def student(name, age, city):
-    print(name, age, city)
-
-# Positional arguments
-student("Amarnath", 21, "Hyderabad")
-
-# Keyword arguments
-student(name="Amarnath", age=21, city="Hyderabad")
-
-#Keyword arguments are more readable because they specify parameter names, making it clear what each value represents.
+# #1.
+# def send_email(to,subject,body):
+#     print(f"{to} {subject} {body}")
+# send_email("amar",body="hi",subject="hello")#amar hello hi
+#
+# #2.
+# def create_profile(username,email,age):
+#     print(f"{username} {email} {age}")
+# create_profile("amar2004",email="drvamarnath2004",age=21)#amar2004 drvamarnath2004 21
+#
+# #3.
+# def create_profile(username,email,age):
+#     print(f"{username} {email} {age}")
+# create_profile(email="drvamarnath2004",age=21,amar2004)#SyntaxError: positional argument follows keyword argument
+#
+# 4.
+# def book_ticket(mem1,mem2,mem3,num):
+#     print(f"{mem1} {mem2} {mem3} {num}")
+# book_ticket(mem1="Alice",mem2="Delhi",mem3="Mumbai",num=2)#Alice Delhi Mumbai 2
+#
+# #5.
+# def student(name, age, city):
+#     print(name, age, city)
+#
+# # Positional arguments
+# student("Amarnath", 21, "Hyderabad")
+#
+# # Keyword arguments
+# student(name="Amarnath", age=21, city="Hyderabad")
+#
+# #Keyword arguments are more readable because they specify parameter names, making it clear what each value represents.
