c1a12a46d5f22c4b1d476878688558c55e3d432d
DRVAmarnath2004 <amarnath.22bce8654@vitapstudent.ac.in>
2026-06-02 10:47:26 +0530

    feat:1st Upload14

--- Positional_arguments.py
+++ Positional_arguments.py
@@ -1,25 +1,25 @@
-#1.
-def fun(name,city,hobby):
-    print(f"{name} {city} {hobby}")
-fun("Amar","Piduguralla","reading")#Amar Piduguralla reading
-fun("Piduguralla","amar","dancing")#Piduguralla amar dancing
-
-#2.
-def sub(a,b):
-    print(a-b)
-sub(10,3)#7
-sub(3,10)#-7
-
-#3.
-Positional arguments are values passed to a function in a specific order.
-The function assigns each value to parameters based on its position in the function call.
-
-#4.
-def bio(first_name,last_name,age):
-    print(f"{first_name} {last_name} {age}")
-bio("Amarnath","Devarsetty",21)#Amarnath Devarsetty 21
-
-#5.
-def add(a,b,c):
-    print(a+b+c)
-add(1,2,3,4)#TypeError: add() takes 3 positional arguments but 4 were given
+# #1.
+# def fun(name,city,hobby):
+#     print(f"{name} {city} {hobby}")
+# fun("Amar","Piduguralla","reading")#Amar Piduguralla reading
+# fun("Piduguralla","amar","dancing")#Piduguralla amar dancing
+#
+# #2.
+# def sub(a,b):
+#     print(a-b)
+# sub(10,3)#7
+# sub(3,10)#-7
+#
+# #3.
+# Positional arguments are values passed to a function in a specific order.
+# The function assigns each value to parameters based on its position in the function call.
+#
+# #4.
+# def bio(first_name,last_name,age):
+#     print(f"{first_name} {last_name} {age}")
+# bio("Amarnath","Devarsetty",21)#Amarnath Devarsetty 21
+#
+# #5.
+# def add(a,b,c):
+#     print(a+b+c)
+# add(1,2,3,4)#TypeError: add() takes 3 positional arguments but 4 were given
