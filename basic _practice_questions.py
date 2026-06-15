b5062f1dd2141848daf437a1a8d194c78df213e2
DRVAmarnath2004 <amarnath.22bce8654@vitapstudent.ac.in>
2026-06-15 12:39:00 +0530

    feat:1st Upload19

--- basic _practice_questions.py
+++ basic _practice_questions.py
@@ -1,9 +1,9 @@
-l = ["Hello", "Hii", "who", "are", "you?"]
+l = ["Hello", "Hii", "Who", "are", "you?"]
 v = "AEIOUaeiou"
 k = list(map(lambda x: ''.join(c for c in x if c not in v), l))
 print(k)
 
-l = ["Hello", "Hii", "who", "are", "you?"]
+l = ["Hello", "Hii", "Who", "are", "you?"]
 v = "AEIOUaeiou"
 k = list(map(lambda x: ''.join(filter(lambda c: c not in v, x)), l))
 m = list(map(lambda x: sum(map(ord, x)), k))
